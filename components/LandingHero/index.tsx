2df859c0446c3995e053e877f86f0a23b9306fa7
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-09-03 00:38:16 +0100

    fix: rectify spacing issue

--- components/LandingHero/index.tsx
+++ components/LandingHero/index.tsx
@@ -4,9 +4,9 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function LandingHero() {
     return (
-        <div className="w-full h-screen flex items-center justify-center my-2">
-            <div className="w-full h-full bg-[url('/background/blocks.png')] bg-cover bg-center flex items-center justify-center md:flex-row flex-col">
-                <div className="w-5/10 md:p-20 p-10">
+        <div className="w-full h-full md:h-screen flex items-center justify-center">
+            <div className="w-full h-full container bg-[url('/background/blocks.png')] bg-cover bg-center flex items-center justify-center md:flex-row flex-col">
+                <div className="md:w-6/12 w-full h-full flex flex-col justify-center p-10">
                     <h1 className="md:text-6xl text-4xl font-bold">Virtual & Augmented Reality in STEM Education for Secondary Schools.</h1>
                     <p className="md:text-2xl text-lg mt-10">Virtual Science Labs offering engaging STEM courses<br /> accessible on tablet or PC anywhere, anytime.</p>
                     <div className="mt-10 flex items-center gap-x-3">
@@ -14,8 +14,8 @@ export default function LandingHero() {
                         <Link href="/features" className="text-blue-500 flex items-center gap-x-2 hover:gap-x-3 transition-all duration-300 font-bold">Watch Video <FaArrowRightLong /></Link>
                     </div>
                 </div>
-                <div className="w-5/10">
-                    <Image src="/stock/young man & woman.png" alt="Hero" width={500} height={500} className="w-full h-full object-contain" />
+                <div className="md:w-6/12 w-full h-full flex items-center justify-center">
+                    <Image src="/stock/young man & woman.png" alt="Hero" width={750} height={750} className="w-[750px] h-[750px] md:w-auto md:h-[80%] object-cover" />
                 </div>
             </div>
         </div>
