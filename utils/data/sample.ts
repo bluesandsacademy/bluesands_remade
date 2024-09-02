e088e6d34e68edc2500ddc2c07e10bceaad70b28
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-09-02 02:51:10 +0100

    feat: created data sample for topnav

---
+++ utils/data/sample.ts
@@ -0,0 +1,40 @@
+export const topNav = [
+    {
+        title: "Home",
+        href: "/"
+    },
+    {
+        title: "Virtual STEM Labs",
+        href: null,
+        dropdown: [
+            {
+                title: "Virtual Biology",
+                href: "/virtual-labs/biology"
+            },
+            {
+                title: "Virtual Chemistry",
+                href: "/virtual-labs/chemistry"
+            },
+            {
+                title: "Virtual Physics",
+                href: "/virtual-labs/physics"
+            }
+        ]
+    },
+    {
+        title: "Features",
+        href: "/features"
+    },
+    {
+        title: "Pricing",
+        href: "/pricing"
+    },
+    {
+        title: "Blog",
+        href: "/blog"
+    },
+    {
+        title: "Contact",
+        href: "/contact"
+    }
+]
