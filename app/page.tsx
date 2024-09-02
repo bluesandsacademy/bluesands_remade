207208bf798b3d713951c208ce604fa5ce6df771
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-09-02 04:17:03 +0100

    feat: set up brands

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import LandingHero from "@/components/LandingHero";
+import Brands from "@/components/Brands";
 
 export default function Home() {
   return (
-    <main className="w-full h-full px-10">
+    <main className="w-full h-full bg-[#F5F8FE]">
       <Navbar />
       <LandingHero />
+      <Brands />
     </main>
   );
 }
