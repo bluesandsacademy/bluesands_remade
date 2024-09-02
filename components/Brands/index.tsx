985ca89b4630dc1c630e20c92d1bfdd39c681374
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-09-03 00:37:41 +0100

    chore: set vertical margin

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -2,7 +2,7 @@ import { brandLogos } from "@/utils/data/sample";
 import Image from "next/image";
 export default function Brands() {
     return (
-        <div className="block md:px-10 px-0 md:py-10 py-0 bg-white w-full overflow-y-scroll">
+        <div className="block md:px-10 px-0 md:py-10 py-0 bg-white w-full my-5">
             <div className="w-full flex items-center justify-center bg-[#F5F8FE] h-40 rounded-lg">
                 {brandLogos.map((logo, index) => (
                     <div key={index} className={`w-full h-24 flex items-center justify-center ${index !== brandLogos.length - 1 ? 'border-r border-gray-300' : ''}`}>
