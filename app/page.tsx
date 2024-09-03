3abdc571bea77e73be699499a1bd6b7ce854400e
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-09-03 19:56:50 +0100

    feat: updated page

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,20 @@ import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import LandingHero from "@/components/LandingHero";
 import Brands from "@/components/Brands";
-
+import Footer from "@/components/Footer";
+import NewsLetterForm from "@/components/NewsLetterForm";
+import WhyBlueSands from "@/components/WhyBlueSands";
+import AccessStats from "@/components/AccessStats";
 export default function Home() {
   return (
     <main className="w-full h-full bg-[#F5F8FE]">
       <Navbar />
       <LandingHero />
       <Brands />
+      <NewsLetterForm />
+      <AccessStats />
+      <WhyBlueSands />
+      <Footer />
     </main>
   );
 }
