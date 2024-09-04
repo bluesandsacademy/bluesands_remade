3fcd2bbaee3cb7c0fb37df523b9b2f12ee00459b
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-09-04 23:45:29 +0100

    feat: put together data

--- utils/data/sample.ts
+++ utils/data/sample.ts
@@ -65,3 +65,61 @@ export const socialMedia = [
         icon: "/icons/social/x.png"
     }
 ]
+
+export const features = [
+    {
+        title: "3D Virtual Experiments & Immersive Interaction",
+        icon: "/feature/icons/vr.png",
+        description: "Looking to achieve your learning outcomes? With Blue Sands STEM Lab, you get that and much more! Offer your students interactive 3D virtual science experiments and realistic lab simulations that enhance time management, lab skills, and knowledge while covering any STEM curriculum."
+    },
+    {
+        title: "Awesome Game-Link Experience",
+        icon: "/feature/icons/game.png",
+        description: "Engage students with game-like online experiment simulations in science without limits. With features like an engaging lab partner named Oxi, helpful hints, and the ability to skip ahead (with progress recorded in reports), you can make science learning intuitive and personalized."
+    },
+    {
+        title: "Virtual Experiments, Adaptive to Any Learning Style",
+        icon: "/feature/icons/learning.png",
+        description: "Engage students with game-like online experiment simulations in science without limits. With features like an engaging lab partner named Oxi, helpful hints, and the ability to skip ahead (with progress recorded in reports), you can make science learning intuitive and personalized."
+    },
+    {
+        title: "Fast, East, & Accurate Reports to Grow",
+        icon: "/feature/icons/grow.png",
+        description: "Gain insights into student progress anytime with comprehensive analytics reports, enabling data-driven teaching decisions."
+    },
+    {
+        title: "Science Experiments Simulations Lab & Blue Sands LMS Integration",
+        icon: "/feature/icons/integration.png",
+        description: "Integrate your LMS with our virtual science lab smoothly and for free within 24 hours. "
+    },
+    {
+        title: "Bilingual Simulation Lab",
+        icon: "/feature/icons/bilingual.png",
+        description: "For higher education digital science lab experiments in English or Arabic, Blue Sands STEM Lab has you covered. We ensure you are set up and ready to go in no time!"
+    },
+    {
+        title: "Virtual Reality Headset",
+        icon: "/feature/icons/headset.png",
+        description: "Blue Sands STEM Lab offers a fully integrated, classroom-ready VR headset with no additional hardware required to get started"
+    },
+    {
+        title: "Headset Management",
+        icon: "/feature/icons/headset_management.png",
+        description: "Our portal gives teachers the control to easily send content, direct, and monitor student attention, maximizing learning time."
+    },
+    {
+        title: "Curriculum Aligned Content",
+        icon: "/feature/icons/curriculum.png",
+        description: "Blue Sands STEM Lab provides thousands of virtual and augmented reality educational resources to enhance your lessons, regardless of the subject."
+    },
+    {
+        title: "Secure Storage & Charging",
+        icon: "/feature/icons/secure_storage.png",
+        description: "All headsets come with a rugged storage and charging case to ensure your devices are always ready when you are."
+    },
+    {
+        title: "Set up & Training",
+        icon: "/feature/icons/setup_training.png",
+        description: "Our technicians set up and configure the headsets for you, and our educational team teaches you how to use them."
+    },
+]
