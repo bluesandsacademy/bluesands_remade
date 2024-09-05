18ac65777bef9d073bf4227cce77f360fe267abd
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-09-05 13:45:27 +0100

    fix: hydration

--- app/signup/layout.tsx
+++ app/signup/layout.tsx
@@ -2,13 +2,19 @@ import type { Metadata } from "next"
 
 export const metadata: Metadata = {
     title: "Signup | BlueSands STEM Labs",
-    description: "Signup to BlueSands STEM Labs", 
+    description: "Signup to BlueSands STEM Labs",
 };
 
-export default function RootLayout({
+export default function SignUpLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return <div>{children}</div>;
+    return (
+        <html>
+            <body>
+                {children}
+            </body>
+        </html>
+    );
 }
