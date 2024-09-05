0442bc948883433029c484232eebb40a050ed4e9
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-09-05 13:24:46 +0100

    feat: include description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export const metadata: Metadata = {
-  title: "BlueSands STEM Labs",
-  description: "Virtual & Augmented Reality in STEM Education for Secondary Schools",
+  title: "BlueSands STEM Labs | Innovative VR/AR STEM Education",
+  description: "Experience cutting-edge Virtual and Augmented Reality in STEM education for secondary schools. Enhance learning with immersive, interactive labs across Physics, Chemistry, and Biology. Boost engagement, improve retention, and prepare students for future careers in science and technology.",
 };
 
 export default function RootLayout({
