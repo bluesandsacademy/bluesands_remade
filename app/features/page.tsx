c5c70840dbeb5bf1bb68597a6c5d31eca279d956
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-09-14 10:05:21 +0100

    feat: updated content

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -3,6 +3,9 @@ import Navbar from "@/components/Navbar"
 import Footer from '@/components/Footer'
 import WhyBlueSands from '@/components/WhyBlueSands'
 import { features } from "@/utils/data/sample"
+import { FaArrowRightLong } from "react-icons/fa6";
+import Link from 'next/link';
+
 
 export default function Features() {
     return (
@@ -11,11 +14,15 @@ export default function Features() {
             {/* Features Hero Section */}
             <section className="w-full md:px-10 px-4 md:h-[400px] h-full py-5 flex flex-col md:flex-row items-center justify-between gap-3 bg-[#F5F8FE]">
                 <div className="md:w-6/12 w-full h-full flex flex-col items-center md:items-start justify-center md:text-left text-center space-y-5">
-                    <h1 className="text-gray-600 text-lg font-semibold">Why BlueSands STEM Labs?</h1>
-                    <p className="md:text-5xl text-lg font-bold">Explore the Future of STEM Education: Unleash the Power of Innovation with Our Cutting-Edge Features</p>
+                    {/* <h1 className="text-gray-600 text-lg font-semibold">Why BlueSands STEM Labs?</h1> */}
+                    <p className="md:text-5xl text-lg font-bold">Unleash the Power of Innovation with Our Cutting-Edge Features</p>
+                    <div className="mt-10 flex items-center gap-x-3">
+                        <Link href="/signup" className="bg-blue-500 text-white px-6 py-3 rounded-md">Sign Up</Link>
+                        <Link href="/features" className="text-blue-500 flex items-center gap-x-2 hover:gap-x-3 transition-all duration-300 font-bold">Watch Video <FaArrowRightLong /></Link>
+                    </div>
                 </div>
                 <div className="md:w-6/12 w-full h-full">
-                    <Image src="/labs/lab space.png" alt="Features Hero Image" width={450} height={450} className="w-full h-full object-contain" />
+                    <Image src="/hero/feature.png" alt="Features Hero Image" width={450} height={450} className="w-full h-full object-contain" />
                 </div>
             </section>
             {/* Features Hero Section */}
